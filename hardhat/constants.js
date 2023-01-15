74191b138c59c96abc6806f3063568bea2b0af60
Gewo <gewitt.hiller@gmail.com>
2023-01-15 20:45:53 +0100

    fakeDAO complete

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x64680A11540Ee82B533197BCFd4E41CF4b23FAB3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
